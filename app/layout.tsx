73186b3a43441eaef823f0b508b040072a22ea10
QuasarX35 <shootingquasar555@gmail.com>
2024-06-13 13:11:40 +0800

    initial Home page components scaffolding

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 "use client"; // Directive indicating that this is a client-side module in Next.js
 
 import { ethers } from "ethers";
-import Image from "next/image";
-import Link from "next/link";
 import React, { createContext, useContext, useEffect, useState } from "react";
 
 import NFTMusicPlayerAbi from "../abi/NFTMusicPlayer.json";
 import NFTMusicPlayerAddress from "../abi/NFTMusicPlayer-address.json";
-import logo from "../public/logo.png";
+import Header from "../components/Header";
 import "./globals.css";
 
 // Extend the Window interface to include the ethereum object
@@ -79,35 +77,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   // Render the component
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true}>
+      <body className="dark" suppressHydrationWarning={true}>
         <div className="text-center">
-          <nav className="bg-secondary text-white p-4">
-            <div className="container mx-auto flex justify-between items-center">
-              <Link className="flex items-center" href="/">
-                <Image alt="Logo" height={40} src={logo} width={40} />
-                <span className="ml-2">Music NFT player</span>
-              </Link>
-              <div className="flex items-center">
-                <Link href="/">Home</Link>
-                <Link href="/tokens">My Tokens</Link>
-                <Link href="/resales">My Resales</Link>
-                {userAccount ? (
-                  <a
-                    className="mx-4"
-                    href={`https://etherscan.io/address/${userAccount}`}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    {userAccount.slice(0, 5) + "..." + userAccount.slice(38, 42)}
-                  </a>
-                ) : (
-                  <button className="bg-white text-black px-4 py-2 rounded" onClick={handleWeb3Connection}>
-                    Connect Wallet
-                  </button>
-                )}
-              </div>
-            </div>
-          </nav>
+          <Header handleWeb3Connection={handleWeb3Connection} userAccount={userAccount} />
           <div>
             {/* NOTE: 
               The Provider component is used to provide the context value to its child components.
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,87 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
-  plugins: [],
+const config = {
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  darkMode: ["class"],
+  plugins: [require("tailwindcss-animate")],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px"
+      }
+    },
     extend: {
-      backgroundImage: {
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))"
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out"
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)"
+      },
+      colors: {
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))"
+        },
+        background: "hsl(var(--background))",
+        border: "hsl(var(--border))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))"
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))"
+        },
+        foreground: "hsl(var(--foreground))",
+        input: "hsl(var(--input))",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))"
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))"
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))"
+        },
+        ring: "hsl(var(--ring))",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))"
+        }
+      },
+      fontSize: {
+        "2xl": ["1.5rem", { lineHeight: "2rem" }],
+        "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
+        "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
+        "5xl": ["3rem", { lineHeight: "1" }],
+        "6xl": ["3.75rem", { lineHeight: "1" }],
+        base: ["1rem", { lineHeight: "1.5rem" }],
+        lg: ["1.125rem", { lineHeight: "1.75rem" }],
+        sm: ["0.875rem", { lineHeight: "1.25rem" }],
+        xl: ["1.25rem", { lineHeight: "1.75rem" }],
+        xs: ["0.75rem", { lineHeight: "1rem" }]
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" }
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" }
+        }
       }
     }
   }
-};
+} satisfies Config;
+
 export default config;
