ccd926220945562294b5c17e7faaec467e14e681
bbkx226 <bbkx226@gmail.com>
2024-06-19 10:26:50 +0800

    Update NFT base URI to use IPFS for music files

--- test/NFTMusicPlayer.test.js
+++ test/NFTMusicPlayer.test.js
@@ -8,7 +8,7 @@ describe("NFTMusicPlayer", function () {
   let nftMarketplace;
   let contractDeployer, musicArtist, buyer1, buyer2, otherUsers;
   let royaltyFeePercentage = toWei(0.01); // 1 ether = 10^18 wei
-  let baseURI = "https://bcd-music-nfts.s3.ap-southeast-1.amazonaws.com/musics/";
+  let baseURI = "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
   let musicPrices = [toWei(1), toWei(2), toWei(3), toWei(4), toWei(5), toWei(6), toWei(7), toWei(8)];
   let contractDeploymentFees = toWei(musicPrices.length * 0.01);
 
