52547f8b87ca53c05fa424186b87f28662504fad
Ang Xu Tian <hoxinla123@gmail.com>
2024-06-26 11:38:59 +0800

    abcdefg

--- app/resales/page.tsx
+++ app/resales/page.tsx
@@ -99,20 +99,20 @@ export default function Resales() {
     const swiper = type === "listed" ? listedSwiper.current : soldSwiper.current;
     if (swiper) {
       swiper.slideTo(index);
-    //   const audio = audioRefs.current[index];
-    //   if (audio.paused) {
-    //     audio.play();
-    //   } else {
-    //     audio.pause();
-    //   }
-    // } else {
-    //   // Pause currently playing audio (if any)
-    //   if (currentAudioIndex !== null) {
-    //     audioRefs.current[currentAudioIndex].pause();
-    //   }
-    //   // Play the clicked audio
-    //   audioRefs.current[index].play();
-    //   setCurrentAudioIndex(index); // Update the current audio index
+      //   const audio = audioRefs.current[index];
+      //   if (audio.paused) {
+      //     audio.play();
+      //   } else {
+      //     audio.pause();
+      //   }
+      // } else {
+      //   // Pause currently playing audio (if any)
+      //   if (currentAudioIndex !== null) {
+      //     audioRefs.current[currentAudioIndex].pause();
+      //   }
+      //   // Play the clicked audio
+      //   audioRefs.current[index].play();
+      //   setCurrentAudioIndex(index); // Update the current audio index
     }
 
     if (type === "listed") {
@@ -169,7 +169,7 @@ export default function Resales() {
               >
                 {resaleItems.map((item, idx) => (
                   <SwiperSlide
-                    className="relative glass grid grid-rows-3 rounded-lg w-148 h-168 md:w-72 md:h-96 lg:w-92 lg:h-104 glass-hover"
+                    className="relative hover:shadow-cyan-600 shadow-lg glass grid grid-rows-3 rounded-lg w-148 h-168 md:w-72 md:h-96 lg:w-92 lg:h-104 glass-hover"
                     key={idx}
                     onClick={() => handleSlideItemClick(idx, "listed")}
                   >
@@ -188,19 +188,21 @@ export default function Resales() {
                         width={120}
                       />
                     </div>
-                    <div className="p-4">
-                      <p className="text-lg font-bold">{item.name}</p>
-                      <p className="mt-2">{ethers.utils.formatEther(item.price)} ETH</p>
+                    <div className="p-4 grid grid-rows-3">
+                      <p className="flex items-center justify-center text-2xl font-bold row-span-2">{item.name}</p>
+                      <div className="mt-2 flex items-center justify-center">
+                        <p className="">{ethers.utils.formatEther(item.price)} ETH</p>
+                      </div>
                     </div>
                   </SwiperSlide>
                 ))}
 
                 <div className="absolute top-1/2 z-10 left-0 transform -translate-y-1/2 -translate-x-full flex items-center justify-center">
-                  <div className="swiper-button-prev w-14 h-14 rounded-full shadow-lg"></div>
+                  <div className="swiper-button-prev w-10 h-10 rounded-full shadow-lg"></div>
                 </div>
 
                 <div className="absolute top-1/2 z-10 right-0 transform -translate-y-1/2 translate-x-full flex items-center justify-center">
-                  <div className="swiper-button-next w-14 h-14 rounded-full shadow-lg"></div>
+                  <div className="swiper-button-next w-10 h-10 rounded-full shadow-lg"></div>
                 </div>
               </Swiper>
             </div>
@@ -234,7 +236,7 @@ export default function Resales() {
                   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-3">
                     {completedSales.map((item, idx) => (
                       <SwiperSlide
-                        className="relative glass grid grid-rows-3 rounded-lg w-148 h-168 md:w-72 md:h-96 lg:w-92 lg:h-104"
+                        className="relative glass grid grid-rows-3 hover:shadow-cyan-600 shadow-lg rounded-lg w-148 h-168 md:w-72 md:h-96 lg:w-92 lg:h-104"
                         key={idx}
                         onClick={() => handleSlideItemClick(idx, "sold")}
                       >
@@ -253,9 +255,11 @@ export default function Resales() {
                             width={120}
                           />
                         </div>
-                        <div className="p-4">
-                          <p className="text-lg font-bold">{item.name}</p>
-                          <p className="mt-2">{ethers.utils.formatEther(item.price)} ETH</p>
+                        <div className="p-4 grid grid-rows-3">
+                          <p className="flex items-center justify-center text-2xl font-bold row-span-2">{item.name}</p>
+                          <div className="mt-2 flex items-center justify-center">
+                            <p className="">{ethers.utils.formatEther(item.price)} ETH</p>
+                          </div>
                         </div>
                       </SwiperSlide>
                     ))}
@@ -267,11 +271,11 @@ export default function Resales() {
                 )}
 
                 <div className="absolute top-1/2 z-10 left-0 transform -translate-y-1/2 -translate-x-full flex items-center justify-center">
-                  <div className="swiper-button-prev w-14 h-14 rounded-full shadow-lg"></div>
+                  <div className="swiper-button-prev w-10 h-10"></div>
                 </div>
 
                 <div className="absolute top-1/2 z-10 right-0 transform -translate-y-1/2 translate-x-full flex items-center justify-center">
-                  <div className="swiper-button-next w-14 h-14 rounded-full shadow-lg"></div>
+                  <div className="swiper-button-next w-10 h-10"></div>
                 </div>
               </Swiper>
             </div>
